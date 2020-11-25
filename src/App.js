c0d38e10dda17bef0b2e5723c3daf6d428cee328
stoic-park <demige79@gmail.com>
2020-11-25 22:23:39 +0900

    :wrench: Fix: App.js
    - 테마 변경 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./button";
 import styled, { ThemeProvider } from "styled-components";
+import { dark, light } from "./theme"; // 환경 구분
 
 function App() {
-  const theme = {
-    colors: {
-      bgColor: "black",
-    },
-  };
+  // const theme = {
+  //   colors: {
+  //     bgColor: "black",
+  //   },
+  // };
+  const [mode, setMode] = useState("light"); // 기본 테마
+  const theme = mode === "light" ? light : dark; // 환경에 맞게 값 변경
+
+  const handleThemeButton = () => setMode(mode === "light" ? "dark" : "light"); // 값에 따라 변경
+
   return (
     <ThemeProvider theme={theme}>
       <S.Main>
-        <button>모드 전환</button>
+        {/* <button>모드 전환</button> */}
+        <Button
+          title={theme === "light" ? "밝은 화면" : "어두운 화면"}
+          click={handleThemeButton}
+        />
       </S.Main>
     </ThemeProvider>
   );
