ba178ee9a09451a62a3f66b4ad03bd06238511b4
stoic-park <demige79@gmail.com>
2020-11-25 22:10:16 +0900

    :pencil: Feat: add theme.js
    - 반전을 줄 색상을 담은 객체 export

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const dark = {
+  colors: {
+    titleColor: "black",
+    bgColor: "blue",
+  },
+};
+
+export const light = {
+  colors: {
+    titleColor: "#blue",
+    bgColor: "black",
+  },
+};
